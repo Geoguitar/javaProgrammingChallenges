d882ace6f5cddaa649bbb8ee04560b33c6c4835a
Geoguitar <geovanedsilveira@gmail.com>
2021-12-12 16:19:07 -0300

    exibindo e substituindo nomes.

--- ColletionsList/src/main/java/ExercicioColletionsList.java
+++ ColletionsList/src/main/java/ExercicioColletionsList.java
@@ -3,29 +3,32 @@ import java.util.List;
 
 public class ExercicioColletionsList {
     public static void main(String[] args) {
-//        1- Criar uma lista e executar as operações abaixo:
+//1- Criar uma lista e executar as operações abaixo:
         List<String> listName = new ArrayList<>();
 
-//        Adicione 5 nomes: Juliana, Pedro, Carlos, Larissa e João.
+//Adicione 5 nomes: Juliana, Pedro, Carlos, Larissa e João.
         listName.add("Juliana");
         listName.add("Pedro");
         listName.add("Carlos");
         listName.add("Larissa");
         listName.add("João");
 
-//        Navegue na lista exibindo cada nome no console.
-        
+//Navegue na lista exibindo cada nome no console.
+        System.out.println(listName);
 
-//        Substitua o nome Carlos por Roberto.
-//        Retorne o nome da posição 1.
-//        Remova o nome da posição 4.
-//        Remova o nome João.
-//        Retorne a quantidade de itens na lista.
-//        Verifique se o nome Juliano existe na lista.
-//        Crie uma nova lista com os nomes: Ismael e Rodrigo.
-//        Adicione todos os itens da nova lista na primeira lista criada.
-//        Ordene os itens da lista por ordem alfabética.
-//        Verifique se a lista está vazia.
+//Substitua o nome Carlos por Roberto.
+        listName.set(2, "Roberto");
+        System.out.println(listName);
+        
+//Retorne o nome da posição 1.
+//Remova o nome da posição 4.
+//Remova o nome João.
+//Retorne a quantidade de itens na lista.
+//Verifique se o nome Juliano existe na lista.
+//Crie uma nova lista com os nomes: Ismael e Rodrigo.
+//Adicione todos os itens da nova lista na primeira lista criada.
+//Ordene os itens da lista por ordem alfabética.
+//Verifique se a lista está vazia.
 
     }
 }
