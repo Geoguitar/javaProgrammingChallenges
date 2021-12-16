84bc6d36a2cbeb2e519c060e6fd36d7048e196cf
Geoguitar <geovanedsilveira@gmail.com>
2021-12-16 15:56:51 -0300

    iniciando o diretório.

---
+++ TeamTask/TaskCollectionList/src/main/java/TaskList.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    public static void main(String[] args) {
+
+//        00-Crie uma lista e execute as operações abaixo;
+
+        List<String> listName = new ArrayList<>();
+
+//        01-Adicione 10 nomes: Juliana,Joana,Claudia,Willian,Paula,Tatiana,Pedro,Carlos,Larissa e João.
+//        02-Navegue na lista exibindo cada nome no console.
+//        03-Substitua o nome Carlos por Roberto.
+//        04-Retorne o nome da posição 1.
+//        05-Remova o nome da posição 4.
+//        06-Remova o nome João.
+//        07-Retorne a quantidade de itens na lista.
+//        08-Verifique se o nome Juliano existe na lista.
+//        09-Crie uma nova lista com os nomes: Ismael e Rodrigo.
+//        10-Adicione todos os itens da nova lista na primeira lista criada.
+//        11-Ordene os itens da lista por ordem alfabética.
+//        12-Verifique se a lista está vazia.
+
+    }
+}
